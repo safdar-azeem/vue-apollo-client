7a5fb66b93dd306ae948f106e10cd1166d3c1a50
safdar-azeem <safdarazeem00@gmail.com>
2026-01-24 04:04:14 +0500

    Add loadApolloClients.ts

---
+++ src/composables/loadApolloClients.ts
@@ -0,0 +1,10 @@
+
+import { getClients } from '../configStore'
+import { provideApolloClients } from '@vue/apollo-composable'
+
+export function loadApolloClients() {
+  const clients = getClients()
+  if (clients) {
+    provideApolloClients(clients)
+  }
+}
