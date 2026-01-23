a74537e0a65a657efa2b08e3dcd11cb31d5fff1a
safdar-azeem <safdarazeem00@gmail.com>
2026-01-24 04:09:35 +0500

    Add types.ts

---
+++ src/vite/types.ts
@@ -0,0 +1,32 @@
+
+export interface VueApolloViteOptions {
+  /**
+   * Path to GraphQL files or pattern
+   * @default 'src/**\/*.{graphql,gql}'
+   */
+  documents?: string | string[]
+
+  /**
+   * Path to the schema file or URL
+   * @default 'http://localhost:4000/graphql'
+   */
+  schema?: string | string[]
+
+  /**
+   * Directory where generated files will be placed.
+   * By default, it generates `generated.ts` next to the usage or a central file.
+   * @default 'src/graphql/generated.ts'
+   */
+  output?: string
+
+  /**
+   * Enable/Disable file watching
+   * @default true
+   */
+  watch?: boolean
+  
+  /**
+   * Additional plugins or config for codegen
+   */
+  codegenConfig?: Record<string, any>
+}
