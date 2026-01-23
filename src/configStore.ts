a64b9762c8945bb7fe30cbcf0828db09b3c9a2c9
safdar-azeem <safdarazeem00@gmail.com>
2026-01-24 04:04:15 +0500

    Add configStore.ts

---
+++ src/configStore.ts
@@ -0,0 +1,23 @@
+
+import { ApolloClient } from '@apollo/client/core'
+import { VueApolloClientOptions } from './types'
+
+let globalConfig: VueApolloClientOptions | null = null
+let globalClients: Record<string, ApolloClient<any>> | null = null
+
+export const setGlobalConfig = (config: VueApolloClientOptions) => {
+  globalConfig = config
+}
+
+export const getGlobalConfig = (): VueApolloClientOptions => {
+  return globalConfig || {
+      endPoints: { default: 'http://localhost:4000/graphql' },
+      tokenKey: 'token',
+  }
+}
+
+export const setClients = (clients: Record<string, ApolloClient<any>>) => {
+  globalClients = clients
+}
+
+export const getClients = () => globalClients
