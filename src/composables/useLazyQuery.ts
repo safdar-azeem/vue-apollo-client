f2225272f40aa066f7e5083457fd670b023f4798
safdar-azeem <safdarazeem00@gmail.com>
2026-01-24 05:19:37 +0500

    Update useLazyQuery.ts

--- src/composables/useLazyQuery.ts
+++ src/composables/useLazyQuery.ts
@@ -1,13 +1,26 @@
-
-import { useLazyQuery as apolloUseLazyQuery, UseQueryReturn, UseQueryOptions } from '@vue/apollo-composable'
+import {
+  useLazyQuery as apolloUseLazyQuery,
+  UseQueryReturn,
+  UseQueryOptions,
+} from '@vue/apollo-composable'
 import { ref, Ref } from 'vue'
 import { OperationVariables } from '@apollo/client/core'
 
-export interface UseLazyQueryReturn<TResult, TVariables> extends UseQueryReturn<TResult, TVariables> {
-  load: (document?: any, variables?: TVariables, options?: UseQueryOptions<TResult, TVariables>) => Promise<any>
+export interface UseLazyQueryReturn<TResult, TVariables> extends UseQueryReturn<
+  TResult,
+  TVariables
+> {
+  load: (
+    document?: any,
+    variables?: TVariables,
+    options?: UseQueryOptions<TResult, TVariables>
+  ) => Promise<any>
 }
 
-export const useLazyQuery = <TResult = any, TVariables extends OperationVariables = OperationVariables>(
+export const useLazyQuery = <
+  TResult = any,
+  TVariables extends OperationVariables = OperationVariables,
+>(
   document: any,
   variables?: TVariables | (() => TVariables),
   options?: UseQueryOptions<TResult, TVariables>
@@ -19,16 +32,16 @@ export const useLazyQuery = <TResult = any, TVariables extends OperationVariable
 
   // Always keep loading as false after initial fetch
   const loadingState = ref(false)
-   
+
   async function fetchOrRefetch(newVariables?: TVariables) {
-      loadingState.value = lazyQuery.result?.value ? false : true
-     
-    try {      
+    loadingState.value = lazyQuery.result?.value ? false : true
+
+    try {
       if (!lazyQuery.result?.value) {
         // @ts-ignore
         await lazyQuery?.load(document, newVariables, options)
         return lazyQuery.refetch(newVariables || variables)
-      } else { 
+      } else {
         return lazyQuery.refetch(newVariables || variables)
       }
     } finally {
