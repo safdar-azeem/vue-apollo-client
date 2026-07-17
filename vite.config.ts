d917e693c5841e5e7df3da0205adc2f906b1a992
safdar-azeem <safdarazeem00@gmail.com>
2026-07-18 01:00:59 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { fileURLToPath } from 'node:url'
 import dts from 'vite-plugin-dts'
+
+const root = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -11,12 +14,13 @@ export default defineConfig({
   build: {
     lib: {
       entry: {
-        'vue-apollo-client': path.resolve(__dirname, 'src/index.ts'),
-        vite: path.resolve(__dirname, 'src/vite/index.ts'),
+        'vue-apollo-client': path.resolve(root, 'src/index.ts'),
+        vite: path.resolve(root, 'src/vite/index.ts'),
       },
       name: 'VueApolloClient',
+      formats: ['es', 'cjs'],
       fileName: (format, entryName) => {
-        const ext = format === 'es' ? 'mjs' : 'js'
+        const ext = format === 'es' ? 'mjs' : 'cjs'
         if (entryName === 'vite') {
           return `vite.${ext}`
         }
@@ -25,9 +29,12 @@ export default defineConfig({
     },
     rollupOptions: {
       external: [
+        /^@apollo\/client(?:\/.*)?$/,
+        '@vue/apollo-composable',
         'vue',
         'vue-router',
         'graphql',
+        'graphql-tag',
         '@graphql-codegen/cli',
         'vite',
         'path',
