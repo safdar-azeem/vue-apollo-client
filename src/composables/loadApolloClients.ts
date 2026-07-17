a9dae68203acaf439dee957f75339fdbb402c5d3
safdar-azeem <safdarazeem00@gmail.com>
2026-07-18 01:00:58 +0500

    Update loadApolloClients.ts

--- src/composables/loadApolloClients.ts
+++ src/composables/loadApolloClients.ts
@@ -2,6 +2,11 @@ import { getClients } from '../configStore'
 import { provideApolloClients } from '@vue/apollo-composable'
 
 export function loadApolloClients() {
+  if (typeof window === 'undefined') {
+    throw new Error(
+      'loadApolloClients() is browser-only. SSR code must use app-provided request-scoped clients.'
+    )
+  }
   const clients = getClients()
   if (clients) {
     provideApolloClients(clients)
